559849b389b2c0c611f609cfdc9cee408840722a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-09-06 15:25:20 -0400

    Hello Today Project Completion

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSHelloToday
-TheCodingSpace Example Hello Today Project
+
+TheCodingSpace Example Hello Today Project: https://coding.space/launchpad/703c1a8d-0c55-4bcb-b3ef-832eec05f134
---
+++ main.js
@@ -0,0 +1,29 @@
+function getCurrentDay() {
+  const currentDate = new Date();
+  const dayNum = currentDate.getDay();
+  let daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return daysOfWeek[dayNum];
+}
+
+function getCurrentTime() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const formattedTime = `${hours > 12 ? hours - 12 : hours}:${String(
+    minutes
+  ).padStart(2, '0')} ${hours > 12 ? 'PM' : 'AM'}`;
+  return formattedTime;
+}
+
+const selectorDay = document.getElementById('d');
+const selectorTime = document.getElementById('t');
+selectorDay.innerHTML = getCurrentDay();
+selectorTime.innerHTML = getCurrentTime();
